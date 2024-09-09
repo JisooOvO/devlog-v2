034ed95ca7e03cb8ecb5ad4dcd8daa4fe428d3f9
Amuz-Jisoo <jisoo@amuz.co.kr>
2024-09-09 16:06:23 +0900

    props children 관련 빌드 이슈 수정

--- src/lib/components/reduxProvider.tsx
+++ src/lib/components/reduxProvider.tsx
@@ -5,18 +5,16 @@ import store from "../store";
 import BlogHeader from "./blogHeader";
 import ClientCategoryMaker from "./client/clientCategoryMaker";
 import ClientScrollMaker from "./client/clientScrollMaker";
+import { RootLayoutProps } from "@/app/layout";
 
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function ReduxProvider({ children }: Props) {
+const ReduxProvider = ({ children }: RootLayoutProps) => {
   return (
     <Provider store={store}>
       <ClientCategoryMaker />
       <ClientScrollMaker />
       <BlogHeader />
-      <div className="contents">{children}</div>
     </Provider>
   );
-}
+};
+
+export default ReduxProvider;
