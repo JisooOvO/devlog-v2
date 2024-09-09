87976162887bbf9914398f42335923d2234a0a8f
Amuz-Jisoo <jisoo@amuz.co.kr>
2024-09-09 16:57:37 +0900

    구글 소유권 확인 메타 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import ReduxProvider from "@/lib/components/reduxProvider";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "@/style/layout.css";
 
@@ -8,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "기술블로그",
   description: "블로그 설명",
+  other: {
+    "google-site-verification": "00QzbWi0zlxqsygsgzV4unpH7HwdvieHl8XUCk7TMMY",
+  },
 };
 
 export interface RootLayoutProps {
@@ -20,6 +24,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body className={inter.className}>
         <ReduxProvider>
           <div className="contents">{children}</div>
+          <Analytics />
         </ReduxProvider>
       </body>
     </html>
