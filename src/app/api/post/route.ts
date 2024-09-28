df1d96bf37ece0fc563184c07c24bb9742a9394a
jisooOvO <fdking1887@naver.com>
2024-09-29 03:43:16 +0900

    post api 수정

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -3,63 +3,68 @@ import prisma from "@/lib/prisma";
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 import checkPost from "./func/checkPost";
+import { Prisma } from "@prisma/client";
 
 interface Props {
   post: Content;
+  published: boolean;
 }
 
 export async function POST(req: NextRequest) {
-  const { post }: Props = await req.json();
+  const { post, published }: Props = await req.json();
 
-  if (!checkPost(post)) {
+  const [missingItem, flag] = checkPost(post);
+
+  if (published && !flag) {
     return NextResponse.json(
-      { message: "Something is missing" },
+      { message: `${missingItem} 필수 입력 사항입니다.` },
       { status: 400 },
     );
   }
 
-  if (post?.id === undefined) {
-    try {
-      let [user, thumbnail, topic, series] = await prisma.$transaction([
-        prisma.user.findFirst({
-          where: {
-            email: post?.author?.email,
-          },
-        }),
-        prisma.thumbnail.findFirst({
-          where: {
-            path: post?.thumbnail?.path,
-          },
-        }),
-        prisma.topic.findFirst({
-          where: {
-            name: post?.topic?.name,
-          },
-        }),
-        prisma.series.findFirst({
-          where: {
-            name: post?.series?.name,
-          },
-        }),
-      ]);
+  try {
+    let [user, thumbnail, topic, series] = await prisma.$transaction([
+      prisma.user.findFirst({
+        where: {
+          email: post?.author?.email,
+        },
+      }),
+      prisma.thumbnail.findFirst({
+        where: {
+          path: post?.thumbnail?.path,
+        },
+      }),
+      prisma.topic.findFirst({
+        where: {
+          name: post?.topic?.name,
+        },
+      }),
+      prisma.series.findFirst({
+        where: {
+          name: post?.series?.name,
+        },
+      }),
+    ]);
 
-      if (topic === null) {
-        topic = await prisma.topic.create({
-          data: {
-            name: post?.topic?.name as string,
-          },
-        });
-      }
+    if (topic === null) {
+      topic = await prisma.topic.create({
+        data: {
+          name: post?.topic?.name as string,
+        },
+      });
+    }
 
-      if (series === null) {
-        series = await prisma.series.create({
-          data: {
-            topicId: topic.id,
-            name: post?.series?.name as string,
-          },
-        });
-      }
+    if (series === null) {
+      series = await prisma.series.create({
+        data: {
+          topicId: topic.id,
+          name: post?.series?.name as string,
+        },
+      });
+    }
 
+    // 새로 생성
+    if (post?.id === undefined) {
       const newPost = await prisma.post.create({
         data: {
           title: post?.title as string,
@@ -69,57 +74,15 @@ export async function POST(req: NextRequest) {
           thumbnailId: thumbnail?.id,
           topicId: topic.id,
           seriesId: series.id,
-          published: true,
+          published: published,
         },
       });
 
       return NextResponse.json(
-        { message: "Save completed", title: newPost.title },
+        { message: "글 쓰기에 성공하였습니다.", title: newPost.title },
         { status: 200 },
       );
-    } catch (error) {
-      return NextResponse.json(
-        { message: "Save failed", error },
-        { status: 500 },
-      );
-    }
-  } else {
-    try {
-      let [thumbnail, topic, series] = await prisma.$transaction([
-        prisma.thumbnail.findFirst({
-          where: {
-            path: post?.thumbnail?.path,
-          },
-        }),
-        prisma.topic.findFirst({
-          where: {
-            name: post?.topic?.name,
-          },
-        }),
-        prisma.series.findFirst({
-          where: {
-            name: post?.series?.name,
-          },
-        }),
-      ]);
-
-      if (topic === null) {
-        topic = await prisma.topic.create({
-          data: {
-            name: post?.topic?.name as string,
-          },
-        });
-      }
-
-      if (series === null) {
-        series = await prisma.series.create({
-          data: {
-            topicId: topic.id,
-            name: post?.series?.name as string,
-          },
-        });
-      }
-
+    } else {
       const updatedPost = await prisma.post.update({
         where: {
           id: post.id,
@@ -135,15 +98,24 @@ export async function POST(req: NextRequest) {
         },
       });
       return NextResponse.json(
-        { message: "Save completed", title: updatedPost.title },
+        { message: "글 수정에 성공하였습니다.", title: updatedPost.title },
         { status: 200 },
       );
-    } catch (error) {
-      return NextResponse.json(
-        { message: "Save failed", error },
-        { status: 500 },
-      );
     }
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      switch (error.code) {
+        case "P2002":
+          return NextResponse.json(
+            { message: "이미 사용 중인 제목입니다.", error },
+            { status: 500 },
+          );
+      }
+    }
+    return NextResponse.json(
+      { message: "글 쓰기에 실패하였습니다.", error },
+      { status: 500 },
+    );
   }
 }
 
@@ -153,10 +125,7 @@ export async function PUT(req: NextRequest) {
   const token = await getToken({ req });
 
   if (token === null) {
-    return NextResponse.json(
-      { message: "authorization failed" },
-      { status: 401 },
-    );
+    return NextResponse.json({ message: "인증 실패" }, { status: 401 });
   }
 
   try {
@@ -210,13 +179,13 @@ export async function PUT(req: NextRequest) {
     ]);
 
     return NextResponse.json(
-      { message: "Success to update likes", likes: post?._count.likes },
+      { message: "좋아요 수정에 성공하였습니다.", likes: post?._count.likes },
       { status: 200 },
     );
   } catch (error) {
     console.log(error);
     return NextResponse.json(
-      { message: "Failed to update likes" },
+      { message: "좋아요 수정에 실패하였습니다." },
       { status: 500 },
     );
   }
@@ -228,16 +197,16 @@ export async function DELETE(req: NextRequest) {
   const token = await getToken({ req });
 
   if (token === null) {
-    return NextResponse.json(
-      { message: "authorization failed" },
-      { status: 401 },
-    );
+    return NextResponse.json({ message: "인증 실패" }, { status: 401 });
   }
 
   const { post }: { post: Content } = await req.json();
 
   if (post?.author?.email !== token.email) {
-    return NextResponse.json({ message: "User mismatch" }, { status: 401 });
+    return NextResponse.json(
+      { message: "유저 정보가 일치하지 않습니다." },
+      { status: 401 },
+    );
   }
 
   try {
@@ -248,12 +217,12 @@ export async function DELETE(req: NextRequest) {
     });
 
     return NextResponse.json(
-      { message: "Success delete post" },
+      { message: "성공적으로 글 삭제하였습니다." },
       { status: 200 },
     );
   } catch {
     return NextResponse.json(
-      { message: "Failed delete post" },
+      { message: "글 삭제에 실패하였습니다." },
       { status: 500 },
     );
   }
