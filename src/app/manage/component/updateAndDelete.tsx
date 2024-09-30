d391473379484c6bed59ac6e0d8d35c47c815b81
Amuz-Jisoo <jisoo@amuz.co.kr>
2024-09-30 18:45:01 +0900

    블로그 관리 기능 구현

---
+++ src/app/manage/component/updateAndDelete.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import IconButton from "@/lib/components/iconButton";
+import DeleteIcon from "@/lib/icons/delete";
+import "@/style/manage.css";
+import { Item } from "../page";
+import EditIcon from "@/lib/icons/edit";
+import CustomModal from "@/lib/components/customModal";
+import { Dispatch, Fragment, SetStateAction, useState } from "react";
+
+const size = "1.5rem";
+
+interface Props {
+  canUpdate: boolean;
+  canDelete: boolean;
+  item: Item;
+  modelName: string;
+}
+
+const UpdateAndDelete: React.FC<Props> = ({
+  canUpdate,
+  canDelete,
+  item,
+  modelName,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <>
+        {canUpdate ? (
+          <IconButton
+            description="수정하기"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          >
+            <EditIcon width={size} height={size} />
+          </IconButton>
+        ) : null}
+        {canDelete ? (
+          <IconButton
+            description="삭제하기"
+            onClick={() => {
+              const lowerModelName = modelName.toLocaleLowerCase();
+              if (confirm("정말로 삭제하시겠습니까?")) {
+                const fetchData = async () => {
+                  const response = await fetch(`/api/${lowerModelName}`, {
+                    method: "DELETE",
+                    body: JSON.stringify({
+                      [lowerModelName]: { ...item },
+                    }),
+                  });
+
+                  const jsonData = await response.json();
+
+                  alert(jsonData["message"]);
+
+                  switch (response.status) {
+                    case 200:
+                      window.location.reload();
+                      break;
+                  }
+                };
+
+                fetchData();
+              }
+            }}
+          >
+            <DeleteIcon width={size} height={size} />
+          </IconButton>
+        ) : null}
+      </>
+      <CustomModal isOpen={isOpen}>
+        <UpdateModal item={item} modelName={modelName} setIsOpen={setIsOpen} />
+      </CustomModal>
+    </>
+  );
+};
+
+// --------------------------------------------------------------------------
+
+interface UpdateModalProps {
+  item: Item;
+  modelName: string;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const UpdateModal: React.FC<UpdateModalProps> = ({
+  item,
+  modelName,
+  setIsOpen,
+}) => {
+  const [target, setTarget] = useState<Item>(item);
+
+  return (
+    <div>
+      <h2>{modelName} 수정</h2>
+      <div className="update-container">
+        {Object.keys(item).map((key, idx) => {
+          const propsKey = `${key}${idx}`;
+          if (key.toLocaleLowerCase().includes("id") || key.includes("At"))
+            return <Fragment key={propsKey}></Fragment>;
+          return (
+            <div key={propsKey}>
+              <p className="update-key">{key}</p>
+              <input
+                className="update-input"
+                type="text"
+                value={target[key as keyof Item]?.toString()}
+                onChange={(event) => {
+                  if (typeof item[key as keyof Item] === "string") {
+                    setTarget((prev) => {
+                      const updatedValue = event.target.value;
+                      return {
+                        ...prev,
+                        [key]:
+                          typeof prev[key as keyof Item] === "string"
+                            ? updatedValue
+                            : prev[key as keyof Item],
+                      };
+                    });
+                  }
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="update-button-container">
+        <button
+          className="custom-button"
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          취소
+        </button>{" "}
+        <button
+          className="custom-button"
+          onClick={() => {
+            const lowerModelName = modelName.toLocaleLowerCase();
+            const fetchData = async () => {
+              const response = await fetch(`/api/${lowerModelName}`, {
+                method: "PUT",
+                body: JSON.stringify({
+                  [lowerModelName]: { ...target },
+                }),
+              });
+
+              const jsonData = await response.json();
+
+              alert(jsonData["message"]);
+
+              switch (response.status) {
+                case 200:
+                  window.location.reload();
+                  break;
+              }
+            };
+
+            fetchData();
+          }}
+        >
+          수정
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateAndDelete;
--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,5 +1,123 @@
-const ManagePage = () => {
-  return <p>준비 중</p>;
+import prisma from "@/lib/prisma";
+import { Post, Series, Thumbnail, Topic } from "@prisma/client";
+import "@/style/manage.css";
+import { Fragment } from "react";
+import UpdateAndDelete from "./component/updateAndDelete";
+
+const ManagePage = async () => {
+  const [topics, series, thumbnails, posts] = await prisma.$transaction([
+    prisma.topic.findMany({
+      orderBy: {
+        name: "asc",
+      },
+    }),
+    prisma.series.findMany({
+      orderBy: {
+        name: "asc",
+      },
+    }),
+    prisma.thumbnail.findMany({
+      orderBy: {
+        name: "asc",
+      },
+    }),
+    prisma.post.findMany({
+      orderBy: {
+        title: "asc",
+      },
+    }),
+  ]);
+
+  return (
+    <div>
+      <ManageConatiner
+        model={topics}
+        modelName="Topics"
+        canUpdate={true}
+        canDelete={true}
+      />
+      <ManageConatiner
+        model={series}
+        modelName="Series"
+        canUpdate={true}
+        canDelete={true}
+      />
+      <ManageConatiner
+        model={thumbnails}
+        modelName="Thumbnails"
+        canUpdate={false}
+        canDelete={true}
+      />
+      <ManageConatiner
+        model={posts}
+        modelName="Post"
+        canUpdate={true}
+        canDelete={true}
+      />
+    </div>
+  );
+};
+
+// --------------------------------------------------------------------------
+
+export type Item = Topic | Series | Thumbnail | Post;
+
+interface Props {
+  model: Array<Item> | undefined | null;
+  modelName: string;
+  canUpdate: boolean;
+  canDelete: boolean;
+}
+
+const ManageConatiner: React.FC<Props> = ({
+  model,
+  modelName,
+  canUpdate,
+  canDelete,
+}) => {
+  if (!model) return;
+  return (
+    <>
+      {model.length !== 0 ? (
+        <div className="manage-table-container">
+          <p className="manage-table-title">{modelName}</p>
+          <div className="manage-table-header">
+            {Object.keys(model[0]).map((key, idx) => {
+              const propsKey = `${key}${idx}`;
+              if (key.toLocaleLowerCase().includes("id") || key === "deletedAt")
+                return <Fragment key={propsKey}></Fragment>;
+              return <p key={propsKey}>{key}</p>;
+            })}
+            {canUpdate ? <p>update</p> : null}
+            {canDelete ? <p>delete</p> : null}
+          </div>
+          {model.map((item, index) => (
+            <div key={`manage-${index}`} className="manage-table">
+              {Object.keys(item).map((key, idx) => {
+                const propsKey = `${key}item${idx}`;
+                if (
+                  key.toLocaleLowerCase().includes("id") ||
+                  key === "deletedAt"
+                )
+                  return <Fragment key={propsKey}></Fragment>;
+                return (
+                  <p key={propsKey}>
+                    {item[key as keyof Item]?.toLocaleString()}
+                  </p>
+                );
+              })}
+              <UpdateAndDelete
+                canUpdate={canUpdate}
+                canDelete={canDelete}
+                item={item}
+                modelName={modelName}
+              />
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default ManagePage;
